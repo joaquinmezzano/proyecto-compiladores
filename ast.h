02baa80e526f81727a0bb5914feec7e088e3bebe
joaquinmezzano <mezzanojoaquin@gmail.com>
2025-09-12 16:52:04 -0300

    Initial ast.h configuration

---
+++ ast.h
@@ -0,0 +1,71 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stdio.h>
+
+typedef enum {
+    NODO_PROG,
+    NODO_DECL,
+    NODO_SENT,
+    NODO_ASSIGN,
+    NODO_RETURN,
+    NODO_OP,
+    NODO_INTEGER,
+    NODO_BOOL,
+    NODO_ID
+} TipoNodo;
+
+typedef enum {
+    TOP_SUMA,
+    TOP_RESTA,
+    TOP_MULT,
+    TOP_DIV,
+    TOP_RESTO,
+    TOP_IGUAL,
+    TOP_MAYOR,
+    TOP_MENOR,
+    TOP_MAYORIG,
+    TOP_MENORIG,
+    TOP_DESIGUAL,
+    TOP_COMP,
+    TOP_AND,
+    TOP_OR
+} TipoOP;
+
+typedef struct Nodo {
+    TipoNodo tipo;
+    struct Nodo *padre;
+    struct Nodo *siguiente;
+
+    union {
+        int val_int;
+        int val_bool;
+        char *nombre;
+
+        struct {
+            struct Nodo *izq;
+            struct Nodo *der;
+            TipoOP op;
+        } opBinaria;
+
+        struct {
+            char *id;
+            struct Nodo *expr;
+        } assign;
+
+        struct Nodo *ret_expr;
+    };
+} Nodo;
+
+Nodo *nodo_ID(char *nombre);
+Nodo *nodo_bool(int val_bool);
+Nodo *nodo_integer(int val_int);
+Nodo *nodo_op(TipoOP op, Nodo *izq, Nodo *der);
+Nodo *nodo_return(Nodo *ret_expr);
+Nodo *nodo_assign(char *id, Nodo *expr);
+Nodo *nodo_decl(char *id, Nodo *expr);
+
+void imprimir_nodo(Nodo *nodo, int indent);
+void nodo_libre(Nodo *nodo);
+
+#endif
