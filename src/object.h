b2e5b507513344604322ff8da754bbb656101eef
joaquinmezzano <mezzanojoaquin@gmail.com>
2025-10-12 15:17:45 -0300

    structures defined in .h

--- src/object.h
+++ src/object.h
@@ -1,42 +1,22 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-typedef enum {
-    O_LOAD,
-    O_STORE,
-    O_ADD,
-    O_SUB,
-    O_UMINUS,
-    O_MUL,
-    O_DIV,
-    O_MOD,
-    O_AND,
-    O_OR,
-    O_NOT,
-    O_EQ,
-    O_NEQ,
-    O_LT,
-    O_LE,
-    O_GT,
-    O_GE,
-    O_LABEL,
-    O_GOTO,
-    O_IF_FALSE,
-    O_IF_TRUE,
-    O_RETURN,
-    O_CALL,
-    O_METHOD,
-    O_EXTERN,
-    O_PARAM,
-    O_CALL_PARAM
-} OInstr;
+typedef struct {
+    char **lines;
+    int size;
+    int capacity;
+} ObjectCode;
 
-// PROLOGO
-//     CODIGO
-//     Registros de 8 en 8
-// EPILOGO
+typedef struct {
+    char *name;
+    int offset;
+} VarInfo;
 
-void o_print();
-void o_save_to_file();
+typedef struct {
+    VarInfo *vars;
+    int count;
+    int capacity;
+    int stack_size;
+} VarTable;
 
 #endif
