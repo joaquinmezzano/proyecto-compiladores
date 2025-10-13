3c823caa0403e7203b5550058d7f02b65a09a9aa
tomirodeghiero <tomyrodeghiero@gmail.com>
2025-10-13 15:24:44 -0300

    Added object code and IR translation function declarations

--- src/object.h
+++ src/object.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include "intermediate.h"
+
 typedef struct {
     char **lines;
     int size;
@@ -18,5 +20,23 @@ typedef struct {
     int capacity;
     int stack_size;
 } VarTable;
+
+void object_init(ObjectCode *obj);
+void object_free(ObjectCode *obj);
+void var_table_init(VarTable *table);
+void var_table_free(VarTable *table);
+
+void object_emit(ObjectCode *obj, const char *line);
+int var_table_add(VarTable *table, const char *name);
+int var_table_get_offset(VarTable *table, const char *name);
+int is_temp_var(const char *name);
+int is_const(const char *name);
+int is_label(const char *name);
+const char* get_register_for_temp(const char *temp_name);
+
+void translate_prologue(ObjectCode *obj, const char *func_name, VarTable *vars);
+void translate_epilogue(ObjectCode *obj);
+void translate_ir_instruction(ObjectCode *obj, IRCode *code, VarTable *vars);
+int generate_object_code(const char *ir_filename, const char *output_filename);
 
 #endif
